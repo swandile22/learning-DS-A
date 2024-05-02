7ca88550bfba291928e8ee25ce4e79770609ef98
swandile22 <sandile@spitfireinbound.com>
2024-05-02 23:46:29 +0200

    Bro Code - Learning DSA: Queues

--- src/DataStructuresAndAlgorithms/PriorityQueues/PriorityQueues.java
+++ src/DataStructuresAndAlgorithms/PriorityQueues/PriorityQueues.java
@@ -15,6 +15,7 @@ import java.util.Queue;
 
 public class PriorityQueues {
 
+
     public static void main(String[] args) {
 
         Queue<String> queue = new PriorityQueue<String>(Collections.reverseOrder());
